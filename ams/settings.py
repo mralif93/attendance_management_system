7de8f7a9c594c08cedbee643e4228e35b8d44b19
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-02-26 23:37:22 +0800

    change db settings

--- ams/settings.py
+++ ams/settings.py
@@ -99,18 +99,18 @@ WSGI_APPLICATION = 'ams.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': env("POSTGRES_DATABASE"),
-    #     'USER': env("POSTGRES_USER"),
-    #     'PASSWORD': env("POSTGRES_PASSWORD"),
-    #     'HOST': env("POSTGRES_HOST"),
-    #     'PORT': env("POSTGRES_PORT"),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("POSTGRES_DATABASE"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("POSTGRES_HOST"),
+        'PORT': env("POSTGRES_PORT"),
+    }
 }
 
 
