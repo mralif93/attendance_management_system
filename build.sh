a259f863ab3930dbc85e6686ff2ff0738313753d
Muhammad Alif Bin Tajudin <m.alif.tajudin@muamalat.com.my>
2024-02-28 12:22:55 +0800

    latest udpate

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 echo "BUILD START"
 pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
-python3.9 pip uninstall dataclasses -y
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
